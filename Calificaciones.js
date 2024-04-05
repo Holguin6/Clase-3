f5ef744771b0f113dc3badc4c0c48d6a10ae950b
Isabella <holguindiazisabella0@gmail.com>
2024-04-04 23:33:30 -0500

    Actualización

--- Calificaciones.js
+++ Calificaciones.js
@@ -1,17 +1,20 @@
-function verificarAprobacion(calificacion, asistencias, totalClases) {
-    var porcentajeAsistencias = (asistencias/totalClases)*100;
+function verificarAprobacion(calificacion, asistencias, Total_clases) {
+    var porcentaje = (asistencias / Total_clases) * 100;
 
-    if (porcentajeAsistencias >= 80) {
+    if (porcentaje >= 80) {
         if (calificacion >= 3) { 
             return "Aprobado";
         } else {
             return "Reprobado";
         }
-    } 
+    } else {
+        return "Reprobado por inasistencias";
+    }
 }
+
 let calificacion = parseFloat(prompt("Ingrese la calificación del alumno: "));
 let asistencias = parseFloat(prompt("Ingrese el porcentaje de asistencias del alumno: "));
-let totalClases = parseFloat(prompt("Ingrese el numero total de clases: "));
+let Total_clases = parseFloat(prompt("Ingrese el número total de clases: "));
 
-let resultado = verificarAprobacion(calificacion, asistencias, totalClases);
-alert("El alumno ha "+resultado);
+let resultado = verificarAprobacion(calificacion, asistencias, Total_clases);
+alert("El alumno ha " + resultado);
